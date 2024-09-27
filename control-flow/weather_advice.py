dae92750d752a62ec057e88f0bbfdd28ad4dae31
FelisterNdambia <ndambiafelister914@gmail.com>
2024-09-27 01:48:26 -0700

    correct weather advice

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather=input("Whats the weather like today?(sunny/rainy/cold) ")
+if weather =="sunny":
+    print("wear a tshirt and sunglasses")
+elif weather=="rainy":
+    print("Dont forget your umbrella and raincoat")
+elif weather=="cold":
+    print("make sure to wear a warm coaat and scarf")
+else:
+    print("Sorry , I dont have recommendations for this weather")
+    

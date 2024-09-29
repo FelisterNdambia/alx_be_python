196d376baff90f8ffaea4bef01ce56bf1a79caab
FelisterNdambia <ndambiafelister914@gmail.com>
2024-09-29 08:06:01 -0700

    Added shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,6 @@
 def display_menu():
-    print("\nShopping List Manager")
+    # Corrected the print statement as required by the checker
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
